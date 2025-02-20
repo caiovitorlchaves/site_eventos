9da1dccea34ea729928c6b72931033e59180a670
Caio Vitor Lopes Chaves <caiovitorlchaves@gmail.com>
2025-02-20 13:18:19 -0300

    feat: Implementing eventos_link_repository

--- src/model/entities/__init__.py
+++ src/model/entities/__init__.py
@@ -1,3 +1,4 @@
 from .eventos import Eventos
 from .inscritos import Inscritos
+from .eventos_link import EventosLink
 #A colocação dessas importação neste arquivo init, é para mostrar a tabela de inscritos que a tabela de Eventos existe
